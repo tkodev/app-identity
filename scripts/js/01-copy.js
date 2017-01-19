6ace11b1202e9554b183842f2dc52107685fd8f7
Tony Ko <htko89@gmail.com>
2017-01-19 02:44:44 -0500

    Update: New fs-extra version, front matter parser, preserve timestamp

--- scripts/js/01-copy.js
+++ scripts/js/01-copy.js
@@ -7,6 +7,7 @@ var path = require("path");
 var fs = require("fs-extra");
 var globby = require("globby");
 var minimatch = require("minimatch")
+var matter = require('gray-matter');
 
 // function
 function copy(conf, callback) {
@@ -14,14 +15,19 @@ function copy(conf, callback) {
   var paths = globby.sync(conf.filters.projects);
   for (var i in paths) {
     var mdPath = paths[i];
+    // matter.read(mdPath).data;
     var mdDestPath = path.join(conf.hugo.projects, path.relative(conf.root.projects, mdPath));
     var assetPath = path.join(path.dirname(mdPath), "assets");
     var assetDestPath = path.join(conf.hugo.projects, path.relative(conf.root.projects, assetPath));
-    fs.copySync(mdPath, mdDestPath, function(file) {
+    fs.copySync(mdPath, mdDestPath, {preserveTimestamps: true, filter: function(file) {
       return minimatch(file, "**/.*") ? false : true;
+    }}, function(err){
+      if (err) return console.error(err)
     })
-    fs.copySync(assetPath, assetDestPath, function(file) {
+    fs.copySync(assetPath, assetDestPath, {preserveTimestamps: true, filter: function(file) {
       return minimatch(file, "**/.*") ? false : true;
+    }}, function(err){
+      if (err) return console.error(err)
     })
   }
   callback();
