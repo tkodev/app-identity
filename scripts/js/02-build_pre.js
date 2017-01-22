0b5be80a3042d7ad07887837c2aa0083af877351
Tony Ko <htko89@gmail.com>
2017-01-22 07:06:30 -0500

    Add: Initial commit of v2 styling

--- scripts/js/02-build_pre.js
+++ scripts/js/02-build_pre.js
@@ -4,7 +4,7 @@
 // global node modules
 var gulp = require("gulp");
 var path = require("path");
-var fs = require("fs-extra");
+var fs = require("graceful-fs-extra");
 var rename = require("gulp-rename");
 var sass = require("gulp-sass");
 var postcss = require('gulp-postcss');
@@ -13,8 +13,9 @@ var postcss = require('gulp-postcss');
 // bulma function
 function bulma(conf, done) {
   fs.removeSync(path.join(conf.hugo.static, "bulma-0.3.1/css")); // triggers watch
-  gulp.src(path.join(conf.hugo.source, "bulma-0.3.1/bulma.sass"))
+  gulp.src(path.join(conf.hugo.source, "bulma-0.3.1/bulma-custom.sass"))
     .pipe(rename({
+      basename: "bulma",
       suffix: '.min'
     }))
     .pipe(sass({
@@ -27,5 +28,22 @@ function bulma(conf, done) {
     .on("end", done);
 }
 
+function htko(conf, done) {
+  fs.removeSync(path.join(conf.hugo.static, "htko-framework/css")); // triggers watch
+  gulp.src(path.join(conf.hugo.source, "htko-framework/htko.scss"))
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    // .pipe(sourcemaps.init()) // triggers watch (?)
+    .pipe(postcss([require('autoprefixer')]))
+    // .pipe(sourcemaps.write('.')) // triggers watch
+    .pipe(gulp.dest(path.join(conf.hugo.static, "htko-framework/css"))) // triggers watch
+    .on("end", done);
+}
+
 // exports
 module.exports.bulma = bulma;
+module.exports.htko = htko;
