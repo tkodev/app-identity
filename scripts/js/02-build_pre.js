415c55a37b286d90edd4ab1e9e99302ce6f07854
Tony Ko <htko89@gmail.com>
2017-01-21 19:11:56 -0500

    Fix: Bouncing gulp.watch

--- scripts/js/02-build_pre.js
+++ scripts/js/02-build_pre.js
@@ -8,11 +8,10 @@ var fs = require("fs-extra");
 var sass = require("gulp-sass");
 var rename = require("gulp-rename");
 var postcss = require('gulp-postcss');
-var sourcemaps = require('gulp-sourcemaps');
 
 // bulma function
 function bulma(conf, done) {
-  fs.removeSync(path.join(conf.hugo.static, "bulma-0.3.1/css")); // triggers watch
+  // fs.removeSync(path.join(conf.hugo.static, "bulma-0.3.1/css")); // triggers watch
   gulp.src(path.join(conf.hugo.source, "bulma-0.3.1/bulma.sass"))
     .pipe(rename({
       suffix: '.min'
@@ -20,9 +19,7 @@ function bulma(conf, done) {
     .pipe(sass({
       outputStyle: 'compressed'
     }).on('error', sass.logError))
-    .pipe(sourcemaps.init())
     .pipe(postcss([require('autoprefixer')]))
-    .pipe(sourcemaps.write('.'))
     .pipe(gulp.dest(path.join(conf.hugo.static, "bulma-0.3.1/css"))) // triggers watch x2
     .on("end", done);
 }
