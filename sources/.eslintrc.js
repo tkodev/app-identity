5f1e86f874188a5927952b427ec4978c16615643
Tony Ko <tony@htko.ca>
2019-06-09 05:03:55 -0400

    Add latest linter config

---
+++ sources/.eslintrc.js
@@ -0,0 +1,74 @@
+// dependencies
+const path = require('path');
+
+// export
+module.exports = {
+  "root": true,
+  "extends": [
+    // "plugin:prettier/recommended",
+    "airbnb-base",
+    "plugin:vue/recommended"
+  ],
+  "parserOptions": {
+    "parser": "babel-eslint",
+    "ecmaVersion": 2018,
+    "ecmaFeatures": {
+      "impliedStrict": true,
+      "classes": true
+    }
+  },
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "jquery": true,
+    "jest": true
+  },
+  "rules": {
+    "no-console": "off",
+    "no-new": 0,
+    "no-debugger": 0,
+    "no-alert": 0,
+    "no-await-in-loop": 0,
+    "no-return-assign": [2, "except-parens"],
+    "no-restricted-syntax": [2, "ForInStatement", "LabeledStatement", "WithStatement"],
+    "no-unused-vars": [1, {"ignoreSiblings": true,
+    "argsIgnorePattern": "res|next|^err"}],
+    "prefer-const": [2, {"destructuring": "all"}],
+    "arrow-parens": [2, "always"],
+    "arrow-body-style": [2, "as-needed"],
+    "no-unused-expressions": [2, {"allowTaggedTemplates": true}],
+    "no-param-reassign": [2, {"props": false}],
+    "no-console": 0,
+    "import": 0,
+    "func-names": 0,
+    "space-before-function-paren": 0,
+    "comma-dangle": 0,
+    "max-len": 0,
+    "radix": 0,
+    "no-underscore-dangle": 0,
+    "consistent-return": 0,
+    "import/extensions": 0,
+    "import/prefer-default-export": 0,
+    "import/no-extraneous-dependencies": [2, { "devDependencies": true }],
+    "vue/script-indent": [2, 2, { "baseIndent": 1 }],
+    "no-shadow": [2, {"hoist": "all", "allow": ["resolve", "reject", "done", "next", "err", "error"]}],
+    "quotes": [2, "single", {"avoidEscape": true, "allowTemplateLiterals": true}],
+    // "prettier/prettier": [2, {"trailingComma": "es5", "singleQuote": true, "printWidth": 80, "vueIndentScriptAndStyle": true, "arrowParens": "always"}]
+  },
+  "settings": {
+    "import/resolver": {
+      "webpack": {
+        "config": path.resolve(__dirname, "./frontend/webpack.config.js")
+      }
+    }
+  },
+  "overrides": [
+    {
+      "files": ["*.vue"],
+      "rules": {
+        "indent": "off"
+      }
+    }
+  ]
+}
--- sources/frontend/webpack.config.js
+++ sources/frontend/webpack.config.js
@@ -1,6 +1,6 @@
-// ****************************************************************************************************
+// ************************************************************************************************
 // Init
-// ****************************************************************************************************
+// ************************************************************************************************
 
 // init native dependencies
 const path = require('path');
@@ -11,111 +11,114 @@ const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-
-// ****************************************************************************************************
+// ************************************************************************************************
 // Export Config
-// ****************************************************************************************************
+// ************************************************************************************************
 
-// export config
-module.exports = function(env, argv) {
+// init settings
+const paths = {
+  src: path.resolve(__dirname, './src'),
+  dist: path.resolve(__dirname, './dist'),
+  modules: path.resolve(__dirname, '../node_modules'),
+};
+const appEnv = process.env.NODE_ENV || 'development';
+console.log('ℹ ｢webpack｣:', `Webpack in ${appEnv} mode.`);
 
-  // init settings
-  const paths = {
-    src: path.resolve('./src'),
-    dist: path.resolve('./dist'),
-    modules: path.resolve('../node_modules')
-  };
-  const appEnv = argv.mode || 'development';
-  console.log(`ℹ ｢webpack｣:`, `Webpack in ${appEnv} mode.`);
+// init config
+const config = {
+  performance: { hints: false },
+  stats: appEnv === 'development' ? 'minimal' : true,
+};
 
-  // init config
-  const config = {
-    performance: { hints: false },
-    stats: appEnv === 'development' ? 'minimal' : true,
-  };
+// set up devServer
+config.devServer = {
+  host: 'localhost',
+  port: 8080,
+  open: true,
+  historyApiFallback: true,
+  proxy: {
+    '/api/': 'http://localhost/api/',
+  },
+};
 
-  // set up devServer
-  config.devServer = {
-    host: 'localhost',
-    port: 8080,
-    open: true,
-    historyApiFallback: true,
-    proxy: {
-      '/api/': 'http://localhost/api/'
-    }
-  };
+// set up input and root path
+config.entry = {
+  app: `${paths.src}/entry.js`,
+};
+config.context = paths.src;
 
-  // set up input and root path
-  config.entry = {
-    app: `${paths.src}/entry.js`,
-  };
-  config.context = paths.src;
+// set up path resolvers - path aliases and extensions to search
+config.resolve = {
+  alias: {
+    '@': paths.src,
+    '~': paths.modules,
+    vue$: 'vue/dist/vue.runtime.esm.js', // ensure all instances of vue use the right js file
+  },
+  extensions: ['.scss', '.sass', '.js', '.vue', '.json'],
+};
 
-  // set up path resolvers - path aliases and extensions to search
-  config.resolve = {
-    alias: {
-      '@': paths.src,
-      '~': paths.modules,
-      vue$: 'vue/dist/vue.runtime.esm.js' // ensure all instances of vue use the right js file
-    },
-    extensions: ['.scss', '.sass', '.js', '.vue', '.json']
-  };
-
-  // set up sourcemap and processing modules
-  config.devtool = appEnv === 'development' ? 'source-map' : false,
-  config.module = {
-    rules: [{
+// set up sourcemap and processing modules
+config.devtool = appEnv === 'development' ? 'source-map' : false;
+config.module = {
+  rules: [
+    {
       test: /\.js$/,
       exclude: /node_modules/,
-      use: [
-        { loader: 'babel-loader' }
-      ]
-    }, {
+      use: [{ loader: 'babel-loader' }],
+    },
+    {
       test: /\.vue$/,
-      use: [
-        { loader: 'vue-loader'}
-      ]
-    }, {
+      use: [{ loader: 'vue-loader' }],
+    },
+    {
       test: /\.pug$/,
-      use: [
-        { loader: 'pug-plain-loader' }
-      ]
-    }, {
+      use: [{ loader: 'pug-plain-loader' }],
+    },
+    {
       test: /\.(sa|sc|c)ss$/,
       use: [
-        { loader: appEnv === 'development' ? 'vue-style-loader' : MiniCssExtractPlugin.loader },
-        { loader: 'css-loader'},
-        { loader: 'postcss-loader', options: { plugins: () => [autoprefixer] } },
-        { loader: 'sass-loader', options: { data: '@import "@/css/bootstrap/core.scss";', } }
-      ]
-    }, {
+        {
+          loader:
+            appEnv === 'development'
+              ? 'vue-style-loader'
+              : MiniCssExtractPlugin.loader,
+        },
+        { loader: 'css-loader' },
+        {
+          loader: 'postcss-loader',
+          options: { plugins: () => [autoprefixer] },
+        },
+        {
+          loader: 'sass-loader',
+          options: { data: '@import "@/css/bootstrap/core.scss";' },
+        },
+      ],
+    },
+    {
       test: /\.(svg|png|jpg|gif|txt)$/,
-      use: [
-        { loader: 'file-loader', options: { name: '[path][name].[ext]', } }
-      ]
-    }]
-  };
-
-  // set up plugins
-  config.plugins = [
-    new VueLoaderPlugin(),
-    new MiniCssExtractPlugin({
-      filename: 'css/index.css',
-    }),
-    new HtmlWebpackPlugin({
-      hash: true,
-      template: `${paths.src}/index.html`,
-      filename: `${paths.dist}/index.html`
-    })
-  ];
-
-  // set up output
-  config.output = {
-    path: path.dist,
-    filename: 'js/index.js'
-  };
-    
-  // return
-  return config;
-
+      use: [{ loader: 'file-loader', options: { name: '[path][name].[ext]' } }],
+    },
+  ],
 };
+
+// set up plugins
+config.plugins = [
+  new VueLoaderPlugin(),
+  new MiniCssExtractPlugin({
+    filename: 'css/index.css',
+  }),
+  new HtmlWebpackPlugin({
+    hash: true,
+    template: `${paths.src}/index.html`,
+    filename: `${paths.dist}/index.html`,
+  }),
+];
+
+// set up output
+config.output = {
+  path: path.dist,
+  filename: 'js/index.js',
+};
+
+// export config
+module.exports = config;
