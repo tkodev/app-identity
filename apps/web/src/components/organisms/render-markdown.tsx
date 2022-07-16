4af06a51144791410699e26ee4dc9a7f7a96e4d1
Tony Ko <tony@tko.dev>
2022-07-16 05:36:47 -0400

    add basic paragraph margin

--- apps/web/src/components/organisms/render-markdown.tsx
+++ apps/web/src/components/organisms/render-markdown.tsx
@@ -16,17 +16,17 @@ const RenderMarkdown: React.FC<RenderMarkdownProps> = (props) => {
       <ReactMarkdown
         remarkPlugins={[RemarkGfm]}
         components={{
-          h1: ({ node, ...props }) => <Typography variant="h1" component="p" {...props} />,
-          h2: ({ node, ...props }) => <Typography variant="h2" component="p" {...props} />,
-          h3: ({ node, ...props }) => <Typography variant="h3" component="p" {...props} />,
-          h4: ({ node, ...props }) => <Typography variant="h4" component="p" {...props} />,
-          h5: ({ node, ...props }) => <Typography variant="h5" component="p" {...props} />,
-          h6: ({ node, ...props }) => <Typography variant="h6" component="p" {...props} />,
-          p: ({ node, ...props }) => <Typography variant="body1" component="p" {...props} />,
-          code: ({ node, ...props }) => <Typography variant="body1" component="code" {...props} />,
-          hr: ({ node, ...props }) => <Divider sx={{ my: 2 }} {...props} />,
-          ul: ({ node, ordered, ...props }) => (
-            <Typography variant="body1" component="ul" sx={{ paddingInlineStart: 2 }} {...props} />
+          h1: ({ node, ...rest }) => <Typography variant="h1" component="p" {...rest} />,
+          h2: ({ node, ...rest }) => <Typography variant="h2" component="p" {...rest} />,
+          h3: ({ node, ...rest }) => <Typography variant="h3" component="p" {...rest} />,
+          h4: ({ node, ...rest }) => <Typography variant="h4" component="p" {...rest} />,
+          h5: ({ node, ...rest }) => <Typography variant="h5" component="p" {...rest} />,
+          h6: ({ node, ...rest }) => <Typography variant="h6" component="p" {...rest} />,
+          p: ({ node, ...rest }) => <Typography sx={{ mb: 2 }} variant="body1" component="p" gutterBottom {...rest} />,
+          code: ({ node, ...rest }) => <Typography variant="body1" component="code" {...rest} />,
+          hr: ({ node, ...rest }) => <Divider sx={{ my: 2 }} {...rest} />,
+          ul: ({ node, ordered, ...rest }) => (
+            <Typography variant="body1" component="ul" sx={{ paddingInlineStart: 2 }} {...rest} />
           )
         }}
       >
