a1cbe6b9145bc9443eda8e74166971f2909bab6c
Tony Ko <tony@tko.dev>
2024-04-28 05:52:38 -0400

    fix: hidden gradient

--- apps/kohei.io/src/app/page.tsx
+++ apps/kohei.io/src/app/page.tsx
@@ -1,6 +1,24 @@
 import Image from "next/image";
 import { Card } from "@repo/ui/card";
-import { Gradient } from "../../components/gradient";
+
+function Gradient({
+  conic,
+  className,
+  small,
+}: {
+  small?: boolean;
+  conic?: boolean;
+  className?: string;
+}): JSX.Element {
+  return (
+    <span
+      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${
+        small ? "blur-[32px]" : "blur-[75px]"
+      } ${conic ? "bg-glow-conic" : ""} ${className}`}
+    />
+  );
+}
+
 
 const links = [
   {
@@ -13,24 +31,28 @@ const links = [
 function Page(): JSX.Element {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen p-24">
-      <div className="z-10 items-center justify-center w-full max-w-5xl font-mono text-sm lg:flex">
+      <div className="items-center justify-center w-full max-w-5xl font-mono text-sm lg:flex">
         <p className="fixed top-0 left-0 flex justify-center w-full px-4 pt-8 pb-6 border-b bg-gradient-to-b backdrop-blur-2xl border-neutral-800 bg-zinc-800/30 from-inherit lg:static lg:w-auto lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:bg-zinc-800/30">
           @kohei.io
         </p>
       </div>
 
       <div className="relative flex place-items-center ">
-        <div className="font-sans w-auto py-16 flex justify-between gap-8 items-center flex-col relative z-0">
-          <div className="z-50 flex items-center justify-center w-full">
+        <div className="font-sans w-auto py-16 flex justify-between gap-8 items-center flex-col relative">
+          <Gradient
+            className="top-[-350px] opacity-[0.15] w-[1000px] h-[1000px]"
+            conic
+          />
+          <div className="flex items-center justify-center w-full">
             <div className="absolute min-w-[614px] min-h-[614px]">
               <Image
-                alt="Turborepo"
+                alt="Circles"
                 height={614}
                 src="circles.svg"
                 width={614}
               />
             </div>
-            <div className="absolute z-50 flex items-center justify-center w-64 h-64">
+            <div className="absolute flex items-center justify-center w-64 h-64">
               <Gradient
                 className="opacity-90 w-[120px] h-[120px]"
                 conic
@@ -38,7 +60,7 @@ function Page(): JSX.Element {
               />
             </div>
 
-            <div className="z-50">
+            <div className="z-10">
              <Image
                 alt="Tony's Signature"
                 height={132}
@@ -47,11 +69,7 @@ function Page(): JSX.Element {
               />
             </div>
           </div>
-          <Gradient
-            className="top-[-350px] opacity-[0.15] w-[1000px] h-[1000px]"
-            conic
-          />
-          <div className="z-50 flex flex-col items-center justify-center gap-5 px-6 text-center lg:gap-6">
+          <div className="flex flex-col items-center justify-center gap-5 px-6 text-center lg:gap-6">
             You&apos;ve made it this far. Follow me.
           </div>
         </div>
